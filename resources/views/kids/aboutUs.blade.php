a378c0a23e73ce29cef6acfab3439d49b7240951
Murad Mikayilzadeh <Murad Mikayilzadeh>
2016-12-05 19:44:19 +0400

    12/5/2016 19:44

--- resources/views/kids/aboutUs.blade.php
+++ resources/views/kids/aboutUs.blade.php
@@ -1,6 +1,10 @@
 @extends('layouts.index')
 @section('content')
-
+<style>
+    .degree,.about{
+        font-family: 'Bubblegum Sans', cursive;
+    }
+</style>
 <section id="leisureTime" >
     <div  class="container-fluid">
         <div class="row">
@@ -23,7 +27,7 @@
         
         <div class="col-md-5 col-xs-12" >
             <h2><b>Biz kimik?<b></h2>
-            <p>autism.az portalı tamamilə xeyriyyə məqsədi ilə yaradılmış portaldır. Bu portalda 1943-cü ildə araşdırmalar sayəsində fərq edilən sindrom olan "Autizm" sindromu barəsindədir. Portalın əsas məqasədi psixoloqlar və autizm sindromlu olan insanların əlaqəsini yartamaqdır. Portalda təmamilə psixoloqların məsləhəti ilə yaradılmış interaktiv oyunlar autizm sindromlu insanlara xidmət göstərir. Portal daxilində psixoloqlarla birbaşa əlaqə həmçinin onların məqalələrini oxumaq başqa bur xüsusiyyətimizdir. Paylaşılacaq videolar isə sizi bu sahədə daha da maarifləndirəcək! Gələcəkdə portalda olacaq yeniliklər və funksionallıqlar isə surpriz olaraq qalsın. Bizimlə olmağa davam edin! Hörmətlə, autism.az komandası!</p>
+            <p style="font-size: 17px">autism.az portalı tamamilə xeyriyyə məqsədi ilə yaradılmış portaldır. Bu portalda 1943-cü ildə araşdırmalar sayəsində fərq edilən sindrom olan "Autizm" sindromu barəsindədir. Portalın əsas məqasədi psixoloqlar və autizm sindromlu olan insanların əlaqəsini yartamaqdır. Portalda təmamilə psixoloqların məsləhəti ilə yaradılmış interaktiv oyunlar autizm sindromlu insanlara xidmət göstərir. Portal daxilində psixoloqlarla birbaşa əlaqə həmçinin onların məqalələrini oxumaq başqa bur xüsusiyyətimizdir. Paylaşılacaq videolar isə sizi bu sahədə daha da maarifləndirəcək! Gələcəkdə portalda olacaq yeniliklər və funksionallıqlar isə surpriz olaraq qalsın. Bizimlə olmağa davam edin! Hörmətlə, autism.az komandası!</p>
         </div>
         
     </div>
@@ -78,26 +82,26 @@
                         <div class="col-md-3 col-xs-12 col-sm-12">
                             <img src="assets/images/team1.jpg" alt="">
                             <h4>Murad Mikayilzadə</h4>
-                            <span>Web Developer</span>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos repellendus tenetur atque officiis, doloribus nam aliquam consectetur ipsum eum molestiae sed, quis, voluptatibus. Eum ad, officiis, ullam dignissimos maxime illum!</p>
+                            <span class="degree">Web Developer</span>
+                            <p class="about">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos repellendus tenetur atque officiis, doloribus nam aliquam consectetur ipsum eum molestiae sed, quis, voluptatibus. Eum ad, officiis, ullam dignissimos maxime illum!</p>
                         </div>
                         <div class="col-md-3 col-xs-12 col-sm-12">
                             <img src="assets/images/team2.jpg" alt="">
                             <h4>Murad Mikayilzadə</h4>
-                            <span>Web Developer</span>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo praesentium possimus velit ad maiores doloremque facilis, nemo reiciendis quo pariatur eius sint, earum, modi provident quasi totam amet deleniti explicabo.</p>
+                            <span class="degree">Web Developer</span>
+                            <p class="about">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo praesentium possimus velit ad maiores doloremque facilis, nemo reiciendis quo pariatur eius sint, earum, modi provident quasi totam amet deleniti explicabo.</p>
                         </div>
                         <div class="col-md-3 col-xs-12 col-sm-12">
                             <img src="assets/images/team3.jpg" alt="">
                             <h4>Leyla Abbasova</h4>
-                            <span>Web Developer</span>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam iste numquam assumenda praesentium maxime esse, voluptates vel nemo eius ea aliquid enim optio ad nisi ut pariatur animi, deleniti sunt.</p>
+                            <span class="degree">Web Developer</span>
+                            <p class="about">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam iste numquam assumenda praesentium maxime esse, voluptates vel nemo eius ea aliquid enim optio ad nisi ut pariatur animi, deleniti sunt.</p>
                         </div>
                         <div class="col-md-3 col-xs-12 col-sm-12">
                             <img src="assets/images/team4.jpg" alt="">
                             <h4>Leyla Abbasova</h4>
-                            <span>Web Developer</span>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam iste numquam assumenda praesentium maxime esse, voluptates vel nemo eius ea aliquid enim optio ad nisi ut pariatur animi, deleniti sunt.</p>
+                            <span class="degree">Web Developer</span>
+                            <p class="about">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam iste numquam assumenda praesentium maxime esse, voluptates vel nemo eius ea aliquid enim optio ad nisi ut pariatur animi, deleniti sunt.</p>
                         </div>
                     </div>
         </section>
